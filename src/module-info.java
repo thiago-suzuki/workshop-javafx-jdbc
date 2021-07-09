71f276a2cd70d8f37c847322b433f99baf96ec9e
Thiago Suzuki <thiagoikenaga@gmail.com>
2021-07-08 22:29:56 -0300

    Criação do MainViewController

--- src/module-info.java
+++ src/module-info.java
@@ -3,5 +3,8 @@ module Workshop {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	exports gui;
+	
+	opens gui to javafx.fxml, javafx.graphics;
 	opens application to javafx.graphics, javafx.fxml;
 }

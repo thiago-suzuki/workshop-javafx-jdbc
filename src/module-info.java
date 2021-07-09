bca04aff74a5ecec9de258e5e7d61830dfb93bd8
Thiago Suzuki <thiagoikenaga@gmail.com>
2021-07-08 23:04:02 -0300

    About View

--- src/module-info.java
+++ src/module-info.java
@@ -4,7 +4,9 @@ module Workshop {
 	requires javafx.fxml;
 	
 	exports gui;
+	exports gui.util;
 	
+	opens gui.util to javafx.fxml, javafx.graphics;
 	opens gui to javafx.fxml, javafx.graphics;
 	opens application to javafx.graphics, javafx.fxml;
 }
